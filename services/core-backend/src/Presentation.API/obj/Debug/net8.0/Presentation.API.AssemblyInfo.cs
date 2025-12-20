210bdae55f540e10ec947acc6ee9249f4daff771
furkankuzucuk <149717946+furkankuzucuk@users.noreply.github.com>
2025-12-20 23:45:35 +0300

    react snowfall

--- services/core-backend/src/Presentation.API/obj/Debug/net8.0/Presentation.API.AssemblyInfo.cs
+++ services/core-backend/src/Presentation.API/obj/Debug/net8.0/Presentation.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22327b95211c85da4040fbeb9abe5b54e0ec8831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f09808ef01f582ba714a48d6372a72241092b0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
